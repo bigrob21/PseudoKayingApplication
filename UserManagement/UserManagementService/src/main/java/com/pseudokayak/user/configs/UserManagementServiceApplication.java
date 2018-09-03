a25de2b2511a5c29d7aebab3a9724c83e2c20052
bigrob21 <bigrob21@homtail.com>
2018-09-03 12:00:55 -0400

    cleanup.

--- UserManagement/UserManagementService/src/main/java/com/pseudokayak/user/configs/UserManagementServiceApplication.java
+++ UserManagement/UserManagementService/src/main/java/com/pseudokayak/user/configs/UserManagementServiceApplication.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.assertj.core.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
