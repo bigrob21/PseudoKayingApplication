1fa096447ea13f2695f57871956f95ddd3d6bb23
bigrob21 <bigrob21@homtail.com>
2018-09-03 11:47:39 -0400

    modified basic seeder in the boot app of app, if there are no users then populate a dummy  user.

--- UserManagement/UserManagementService/src/main/java/com/pseudokayak/user/configs/UserManagementServiceApplication.java
+++ UserManagement/UserManagementService/src/main/java/com/pseudokayak/user/configs/UserManagementServiceApplication.java
@@ -1,8 +1,19 @@
 package com.pseudokayak.user.configs;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.assertj.core.util.Arrays;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+
+import com.pseudokayak.user.model.User;
+import com.pseudokayak.user.repository.UserRepository;
 
 @SpringBootApplication
 @Import({ServiceConfig.class, WebConfig.class})
@@ -12,15 +23,22 @@ public class UserManagementServiceApplication {
 		SpringApplication.run(UserManagementServiceApplication.class, args);
 	}
 	
-	/*@Bean
+	@Bean
 	public CommandLineRunner loadData(@Autowired UserRepository repository) {
-		return (args) -> {
-			User user1 = User.builder()
-					.firstName("Robert0")
-					.lastName("Foobar")
-					.build();
-			repository.save(user1);
+			//Only populate or seed if no users are present!
+			return (args) -> {
+				if(repository.findAll().isEmpty()) {
+					List<String> roleList1 = Stream.of("ADMIN", "USER")
+						.map(Object::toString).collect(Collectors.toList());
+					User user1 = User.builder()
+						.userId("robert-0")
+						.firstName("Robert0")
+						.lastName("Foobar")
+						.roles(roleList1)
+						.build();
+					repository.save(user1);
+				}
 		};
-	}*/
+	}
 	
 }
