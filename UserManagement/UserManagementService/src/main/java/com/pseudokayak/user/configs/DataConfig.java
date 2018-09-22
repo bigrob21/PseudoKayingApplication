531d3cb695e1bd2ca0c908ffeb22fc1797d8ea85
bigrob21 <bigrob21@homtail.com>
2018-09-22 09:43:27 -0400

    fixing compile issues in DataConfig, but this is still not ready for prime time as the User Mgmt application is being transitioned from DTO User model in the MongoDB to the original Intended InternalUser model object. Next step is to test the InternalUser and the new converters. The reasoning of moving to explicit converters is to keep the common User model DTO free of Spring Data Mongo or Mongo specific annotations and reduce dependencies on Spring DataMongo on the security commons library.

--- UserManagement/UserManagementService/src/main/java/com/pseudokayak/user/configs/DataConfig.java
+++ UserManagement/UserManagementService/src/main/java/com/pseudokayak/user/configs/DataConfig.java
@@ -1,13 +1,42 @@
 package com.pseudokayak.user.configs;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.mongodb.core.MongoTemplate;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@EnableMongoRepositories(basePackages= {"com.pseudokayak.user.repository"})
-public class DataConfig {
-	
-	public MongoTemplate mongoTemplate(@Autowired(required=true) MongoTemplate template) {
-		template.getConverter().
+import com.mongodb.MongoClient;
+import com.pseudokayak.internal.user.utils.UserReaderConverter;
+import com.pseudokayak.internal.user.utils.UserWriterConverter;
+
+@EnableMongoRepositories(basePackages = { "com.pseudokayak.user.repository" })
+public class DataConfig extends AbstractMongoConfiguration {
+	@Inject
+	private Environment env;
+
+	final List<Converter<?, ?>> converters = new ArrayList<>();
+
+	@Override
+	public MongoClient mongoClient() {
+		return new MongoClient(env.getRequiredProperty("spring.data.mongodb.host"),
+				env.getRequiredProperty("spring.data.mongodb.port", Integer.class));
+	}
+
+	@Override
+	protected String getDatabaseName() {
+		return env.getRequiredProperty("spring.data.mongodb.database");
+	}
+
+	@Override
+	public MongoCustomConversions customConversions() {
+		converters.add(new UserWriterConverter());
+		converters.add(new UserReaderConverter());
+		return new MongoCustomConversions(converters);
 	}
 }
